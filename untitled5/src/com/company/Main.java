96878e2472a765f8add2cdcce67a12610a586248
danil1nanatolyi <80408487+danil1nanatolyi@users.noreply.github.com>
2021-05-02 20:17:16 +0300

    Add files via upload

--- untitled5/src/com/company/Main.java
+++ untitled5/src/com/company/Main.java
@@ -3,22 +3,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        String str =str.nextLine();
-        String sw = "", lw = "";
-        int s = str.length(), l = 0;
-        String words[] = str.split(" Введите слово");
-        for(String word:words){
-            if(word.length()<s)
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        String lw = "", sw="";
+        int s = str.length(), l=0;
+        String words[] = str.split(" Введите строку");
+        for (String word : words) {
+            if (word.length() < s)
                 sw = word;
-            else if(word.length()>l)
+            else if (word.length() >l)
                 lw = word;
         }
 
-        System.out.println("LONG WORD : "+lw);
-        System.out.println("SHORT WORD : "+sw);
+
+        System.out.println("LONGEST WORD : " + lw);
+        System.out.println("SHORTEST WORD : " + sw);
     }
 }
-
-
-
